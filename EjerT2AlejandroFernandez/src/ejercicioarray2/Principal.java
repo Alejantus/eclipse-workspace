b6be620e80ff80036b188f9ce8158cb74a5926fc
Alejandro <alejantus06@gmail.com>
2021-10-26 23:41:46 +0200

    Commit casa array 3

--- EjerT2AlejandroFernandez/src/ejercicioarray2/Principal.java
+++ EjerT2AlejandroFernandez/src/ejercicioarray2/Principal.java
@@ -1,20 +1,19 @@
 package ejercicioarray2;
 
-
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int dos = 2,tam = 10,num = 0;
-		
-		int enteros [] = new int [tam];
-		
-		//int enteros [] = {1,2,3,4,5,6,7,8,2,3};
-		
-		for(int i = 0;i<enteros.length;i++) {
-			enteros[i] *= dos;
-			System.out.println(enteros[i]);
+
+		int dos = 2, tam = 10, mult = 0;
+
+		int enteros[] = new int[tam];
+
+		// int enteros [] = {1,2,3,4,5,6,7,8,2,3};
+
+		for (int i = 1; i < enteros.length; i++) {
+			mult = i * dos;
+			System.out.println(i);
 		}
 
 	}
