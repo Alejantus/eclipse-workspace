74122d632e6a9f0762b237fdecf4a743817ba410
Alejandro <alejantus06@gmail.com>
2021-10-26 14:19:45 +0200

    Programacion commit

---
+++ EjerT2AlejandroFernandez/src/ejercicioarray1/Principal.java
@@ -0,0 +1,16 @@
+package ejercicioarray1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nombres[] = {"Alejandro","Mario","David","Roberto","Jose Joaquín"};
+		
+		for (int i = 0; i < nombres.length; i++) {
+			System.out.println(nombres[i]);
+		}
+
+	}
+
+}
---
+++ EjerT2AlejandroFernandez/src/ejercicioarray2/Principal.java
@@ -0,0 +1,22 @@
+package ejercicioarray2;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int dos = 2,tam = 10,num = 0;
+		
+		int enteros [] = new int [tam];
+		
+		//int enteros [] = {1,2,3,4,5,6,7,8,2,3};
+		
+		for(int i = 0;i<enteros.length;i++) {
+			enteros[i] *= dos;
+			System.out.println(enteros[i]);
+		}
+
+	}
+
+}
