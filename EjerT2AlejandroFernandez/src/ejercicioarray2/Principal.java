f7494bdb0189c6b5b1632815ffe96726ba651eca
Alejandro <alejantus06@gmail.com>
2021-10-27 09:08:33 +0200

    Commit en clase

--- EjerT2AlejandroFernandez/src/ejercicioarray2/Principal.java
+++ EjerT2AlejandroFernandez/src/ejercicioarray2/Principal.java
@@ -6,14 +6,14 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int dos = 2,tam = 10,num = 0;
+		int dos = 2,tam = 10;
 		
 		int enteros [] = new int [tam];
 		
 		//int enteros [] = {1,2,3,4,5,6,7,8,2,3};
 		
 		for(int i = 0;i<enteros.length;i++) {
-			enteros[i] *= dos;
+			i *= dos;
 			System.out.println(enteros[i]);
 		}
 
