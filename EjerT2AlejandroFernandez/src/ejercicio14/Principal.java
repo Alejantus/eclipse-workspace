bb7ce125c333c5c6151ac3bc4af7f71c798581cd
Alejandro <alejantus06@gmail.com>
2021-11-01 21:18:46 +0100

    Commit ejercicios del tema 2 programación

--- EjerT2AlejandroFernandez/src/ejercicio14/Principal.java
+++ EjerT2AlejandroFernandez/src/ejercicio14/Principal.java
@@ -7,25 +7,20 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int opcion = 0,vendido = 0,totalPor = 100, porcen = 20;
-		double sueldo = 1000.0, ventasPor = 0.0,ventas = 0.0,porcentajeC;
+		int opcion = 0, numVentas = 0, porcentaje = 20, totalPorcentaje = 100;
+		double sueldoTotales = 0.0, sueldo = 1000.0, ventasPor = 0,ventas=100.0;
 
-		System.out.println("Bienvenido, vamos a calcular el salario mensual de un vendedor");
+		System.out.println("Bienvenido, en este programa vamos a calcular lo que cobra un vendedor.");
+
 		do {
-			System.out.println("¿Qué acción quieres realizar?");
-			System.out.println("1. Realizar operación");
-			System.out.println("0. Salir");
+			System.out.println("Por favor, pulse un número para seguir con el programa o 0 para salir");
 			opcion = Leer.datoInt();
-			
-
-			System.out.println("¿Cuánto has vendido en total este mes?");
-			vendido = Leer.datoInt();
-			porcentajeC = porcen / totalPor;
-			ventas = sueldo * ventasPor;
-			System.out.printf("Las ventas de este empleado son de: %.2f\n",porcentajeC);
-			
+			System.out.println("¿Cuánto has vendido este mes?");
+			numVentas = Leer.datoInt();
+			ventasPor = (ventas*numVentas)*porcentaje/totalPorcentaje;
+			sueldoTotales = sueldo * ventasPor;
+			System.out.printf("El sueldo de este vendedor es de: %.2f €\n", sueldoTotales);
 		} while (opcion != 0);
-		System.out.println("Hasta luego");
 
 	}
 
--- EjerT2AlejandroFernandez/src/ejercicio16/Principal.java
+++ EjerT2AlejandroFernandez/src/ejercicio16/Principal.java
@@ -1,11 +1,67 @@
 package ejercicio16;
 
+import lectura.Leer;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
+		int opcion = 0;
+		double a = 0, b = 0, suma = 0, resta = 0, multi = 0, divi = 0;
+
 		System.out.println("Bienvenido, en este programa vamos a hacer una mini-calculadora");
+
+		do {
+			System.out.println("\n¿Qué operación quieres realizar?");
+			System.out.println("1. Sumar");
+			System.out.println("2. Restar");
+			System.out.println("3. Multiplicar");
+			System.out.println("4. Dividir");
+			System.out.println("0. Salir");
+			opcion = Leer.datoInt();
+			switch (opcion) {
+			case 1:
+				System.out.println("Introduzca un número");
+				a = Leer.datoDouble();
+				System.out.println("Introduzca un número");
+				b = Leer.datoDouble();
+				suma = a + b;
+				System.out.printf("El resultado es %.2f", suma);
+				break;
+
+			case 2:
+				System.out.println("Introduzca un número");
+				a = Leer.datoDouble();
+				System.out.println("Introduzca un número");
+				b = Leer.datoDouble();
+				resta = a - b;
+				System.out.printf("El resultado es %.2f", resta);
+				break;
+
+			case 3:
+				System.out.println("Introduzca un número");
+				a = Leer.datoDouble();
+				System.out.println("Introduzca un número");
+				b = Leer.datoDouble();
+				multi = a * b;
+				System.out.printf("El resultado es %.2f", multi);
+				break;
+
+			case 4:
+				System.out.println("Introduzca un número");
+				a = Leer.datoDouble();
+				System.out.println("Introduzca un número");
+				b = Leer.datoDouble();
+				divi = a / b;
+				System.out.printf("El resultado es %.2f", divi);
+				break;
+
+			default:
+				System.out.println("Error,operacion no encontrada\n");
+				break;
+			}
+		} while (opcion != 0);
 
 	}
 
