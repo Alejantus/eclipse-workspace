79970203be30a759e2d7ac3e3c12de5fb5a07eb9
Alejandro <alejantus06@gmail.com>
2021-11-02 09:27:44 +0100

    Commit ejercicio 16 bucles

--- EjerT2AlejandroFernandez/src/ejercicio16/Principal.java
+++ EjerT2AlejandroFernandez/src/ejercicio16/Principal.java
@@ -57,7 +57,7 @@ public class Principal {
 					divi = a / b;
 					System.out.printf("El resultado es %.2f", divi);
 				}else {
-					System.out.println("Introduzca un número que no sea cero");
+					System.out.println("Introduzca un número que no sea cer");
 				}
 				break;
 
