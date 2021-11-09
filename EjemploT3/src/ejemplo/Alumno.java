d561b58cc9417909a869734d32f35516ac267e49
Alejandro <alejantus06@gmail.com>
2021-11-09 14:24:32 +0100

    Progamación como instanciar variables

--- EjemploT3/src/ejemplo/Alumno.java
+++ EjemploT3/src/ejemplo/Alumno.java
@@ -10,8 +10,16 @@ public class Alumno {
 	double notaMedia;	
 	
 	//Constructor
+	//NUNCA!!!!! Se lee por teclado en el constructor porque
+	//perdemos toda la libertad
 	public Alumno(int edad,String nombre,String apellidos,double notaMedia) {
 		this.edad=edad;
+		this.nombre=nombre;
+		this.apellidos=apellidos;
+		this.notaMedia=notaMedia;
+	}
+	
+	public Alumno(String nombre,String apellidos,double notaMedia) {
 		this.nombre=nombre;
 		this.apellidos=apellidos;
 		this.notaMedia=notaMedia;
--- EjemploT3/src/ejemplo/Principal.java
+++ EjemploT3/src/ejemplo/Principal.java
@@ -1,15 +1,44 @@
 package ejemplo;
 
+import lectura.Leer;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int edad=0;
+		String nombre,apellido;
+		double notaMedia=0.0;
 		
-		Alumno a1=new Alumno(18,"Alejandro","Fernandez Gomez-Caminero",6.9);
-		Alumno a2=new Alumno(19,"Barbara","Velasco Lopez",8.5);
 		
-		System.out.println(a1.edad);
+		Alumno aEditado,aEditado2;
+		
+		
+		System.out.println("Diga la edad");
+		edad=Leer.datoInt();
+		System.out.println("Diga el nombre");
+		nombre=Leer.dato();
+		System.out.println("Diga el apellido");
+		apellido=Leer.dato();
+		System.out.println("Diga la nota media");
+		notaMedia=Leer.datoDouble();
+		
+		aEditado=new Alumno(edad,nombre,apellido,notaMedia);
+		
 
+		System.out.println("Diga la edad");
+		edad=Leer.datoInt();
+		System.out.println("Diga el nombre");
+		nombre=Leer.dato();
+		System.out.println("Diga el apellido");
+		apellido=Leer.dato();
+		System.out.println("Diga la nota media");
+		notaMedia=Leer.datoDouble();
+		
+		aEditado2=new Alumno(edad,nombre,apellido,notaMedia);
+		
+		System.out.println(aEditado.edad);
+		System.out.println(aEditado2.edad);
 	}
 
 }
