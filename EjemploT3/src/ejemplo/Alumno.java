d47d85df33d416fad3b8b5c3999d212d04f2c3a7
Alejandro <alejantus06@gmail.com>
2021-11-10 10:47:29 +0100

    Commit sobre los getters and setters e iniciacion con métodos

--- EjemploT3/src/ejemplo/Alumno.java
+++ EjemploT3/src/ejemplo/Alumno.java
@@ -3,11 +3,17 @@ package ejemplo;
 public class Alumno {
 	
 	//Atributos
-	
-	int edad;
-	String nombre;
-	String apellidos;
-	double notaMedia;	
+	//Niveles de acceso
+	/**
+	 * public = pueden acceder todos las clases 
+	 * si no hay nada = nada puede acceder a la clase los que no estan en el mismo paquete
+	 * private = solo pueden acceder solo los de esa misma clase
+	 */
+	//TODOS los atributos tendran getters y setters, tambien tendran sus contructores
+	private int edad;
+	private String nombre;
+	private String apellidos;
+	private double notaMedia;	
 	
 	//Constructor
 	//NUNCA!!!!! Se lee por teclado en el constructor porque
@@ -23,6 +29,49 @@ public class Alumno {
 		this.nombre=nombre;
 		this.apellidos=apellidos;
 		this.notaMedia=notaMedia;
+	}
+	
+	//Getters and Setters
+	//En los parentesis de los getters no se escribe nada mientras que en
+	//los setters se tiene que escribir la variable 	
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad=edad;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	public String getApellidos() {
+		return apellidos;
+	}
+	
+	public void setApellidos(String apellidos) {
+		this.apellidos=apellidos;
+	}
+	
+	public double getNotaMedia() {
+		return notaMedia;
+	}
+	
+	public void setNotaMedia(double notaMedia) {
+		this.notaMedia=notaMedia;
+	}
+	
+	//Métodos
+	//Dentro de los parantesis se declara una nueva variable dentro del metodo 
+	public double sumarPuntos(double puntos) {
+		notaMedia+=puntos;
+		
+		return notaMedia;
 	}
 
 }
--- EjemploT3/src/ejemplo/Principal.java
+++ EjemploT3/src/ejemplo/Principal.java
@@ -8,10 +8,10 @@ public class Principal {
 		// TODO Auto-generated method stub
 		int edad=0;
 		String nombre,apellido;
-		double notaMedia=0.0;
+		double notaMedia=0.0,puntos=0.0;
 		
 		
-		Alumno aEditado,aEditado2;
+		Alumno aEditado;
 		
 		
 		System.out.println("Diga la edad");
@@ -25,20 +25,27 @@ public class Principal {
 		
 		aEditado=new Alumno(edad,nombre,apellido,notaMedia);
 		
+		//Los getters se usan para obtener la informacion del metodo
+		
+		System.out.println("Su edad es: "+aEditado.getEdad());
+		System.out.println("Su nombre es: "+aEditado.getNombre());
+		System.out.println("Su apellido es: "+aEditado.getApellidos());
+		System.out.println("Su nota media es de: "+aEditado.getNotaMedia());
 
-		System.out.println("Diga la edad");
-		edad=Leer.datoInt();
-		System.out.println("Diga el nombre");
-		nombre=Leer.dato();
-		System.out.println("Diga el apellido");
-		apellido=Leer.dato();
-		System.out.println("Diga la nota media");
+		//mientras que los setters compruban si son del mismo tipo
+		
+		System.out.println("Diga nueva nota media");
 		notaMedia=Leer.datoDouble();
+		aEditado.setNotaMedia(notaMedia);
+		System.out.println("Su nueva nota media es: "+aEditado.getNotaMedia());
+		notaMedia=1212.5;
+		System.out.println("pero no es lo mismo que la variable notaMedia "
+				+ "si lo cambio a mano que vale: "+notaMedia);
 		
-		aEditado2=new Alumno(edad,nombre,apellido,notaMedia);
-		
-		System.out.println(aEditado.edad);
-		System.out.println(aEditado2.edad);
+		System.out.println("¿Cuántos puntos quieres sumar?");
+		puntos=Leer.datoDouble();
+		aEditado.sumarPuntos(puntos);
+		System.out.println(aEditado.getNotaMedia());
 	}
 
 }
